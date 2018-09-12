d4d7509d568fd4b725ec258b887005b807fb6311
Miguel <miguel@sopinet.com>
2018-09-12 18:30:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Práctica Frontend Avanzado
+
+## Installation
+
+```
+npm install
+npm build
+```
+
+## Development
+
+```
+npm build
+npm run start:dev
+```
+
+## Production
+
+```
+npm build
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Create new page function
+var page = function ({ title, chunks, template, filename }) {
+  return new HtmlWebpackPlugin({
+    title: title,
+    chunks: chunks,
+    template: template,
+    filename: filename
+  })
+};
+
+var commonConfig = {
+  entry: {
+    articles: path.join(__dirname, 'src', 'pages', 'articles', 'index')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][hash].js'
+  },
+  plugins: [
+    page({
+      title: 'Articles',
+      chunks: ['articles'],
+      template: path.join(__dirname, 'src', 'pages', 'articles', 'index.html'),
+      filename: path.resolve(__dirname, 'dist', 'index.html')
+    })
+  ]
+};
+
+const devConfig = {
+
+};
+
+const prodConfig = {
+  plugins: [
+    new CleanWebpackPlugin(path.join(__dirname, 'dist'))
+  ]
+};
+
+module.exports = (env, argv) =>
+  argv.mode === 'development' ?
+      commonConfig :
+      merge(commonConfig, prodConfig);
