d2f23bf90b486a26e5683e4cf744cfeee3525c7e
Miguel <miguel@sopinet.com>
2018-09-14 20:41:47 +0200

    Continue with configuration of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ var path = require('path');
 var merge = require('webpack-merge');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var Dotenv = require('dotenv-webpack');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
+require("@babel/polyfill");
 
 // Create new page function
 var page = function ({title, chunks, template, filename}) {
@@ -11,13 +16,16 @@ var page = function ({title, chunks, template, filename}) {
     title: title,
     chunks: chunks,
     template: template,
-    filename: filename
+    filename: filename,
+    minify: {
+      collapseWhitespace: true
+    }
   });
 };
 
 var commonConfig = {
   entry: {
-    articles: path.join(__dirname, 'src', 'pages', 'articles', 'index')
+    articles: ["@babel/polyfill", path.join(__dirname, 'src', 'pages', 'articles', 'index')]
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -31,18 +39,91 @@ var commonConfig = {
       template: path.join(__dirname, 'src', 'pages', 'articles', 'index.html'),
       filename: path.resolve(__dirname, 'dist', 'index.html')
     })
-  ]
+  ],
+  module:{
+    rules: [
+      {
+        test: /\.(html|ejs)$/,
+        use: ['html-loader', 'ejs-html-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src', 'components'),
+      styles: path.resolve(__dirname, 'src', 'styles')
+    }
+  }
 };
 
-const devConfig = {};
+const devConfig = {
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
+  }
+};
 
 const prodConfig = {
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(
+          {
+            cssProcessorOptions: {map: {inline: false}}
+          }
+      ),
+      new UglifyJsPlugin(
+          {
+            cache: true,
+            parallel: true,
+            sourceMap: true, // set to true if you want JS source maps
+            extractComments: true
+          }
+      )
+    ]
+  },
   plugins: [
-    new CleanWebpackPlugin(path.join(__dirname, 'dist'))
-  ]
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
+    }),
+    new CleanWebpackPlugin(path.join(__dirname, 'dist')),
+    new CriticalPlugin({
+      src: path.join(__dirname, 'src', 'pages', 'articles', 'index.html'),
+      inline: true,
+      minify: true,
+      dest: path.join(__dirname, 'dist', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  }
 };
 
 module.exports = (env, argv) =>
     argv.mode === 'development' ?
-        commonConfig :
+        merge(commonConfig, devConfig) :
         merge(commonConfig, prodConfig);
